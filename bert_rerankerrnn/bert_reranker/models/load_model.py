a3aa732b175cd4886f7e62104b2188c1cd7f15bd
ashitadiwan96 <37137291+ashitadiwan96@users.noreply.github.com>
2020-04-30 19:55:05 -0400

    Add files via upload

---
+++ bert_rerankerrnn/bert_reranker/models/load_model.py
@@ -0,0 +1,45 @@
+from bert_reranker.models.bert_encoder import BertEncoder
+from bert_reranker.models.cnn_model import CNNEncoder
+from bert_reranker.models.retriever import Retriever, EmbeddingRetriever, FeedForwardRetriever, CNNRetriever
+from bert_reranker.utils.hp_utils import check_and_log_hp
+
+
+def load_model(hyper_params, tokenizer, debug):
+    check_and_log_hp(['name'], hyper_params['model'])
+    if hyper_params['model']['name'] == 'bert_encoder':
+        bert_question_encoder = BertEncoder(hyper_params)
+        bert_paragraph_encoder = BertEncoder(hyper_params)
+        model = EmbeddingRetriever(
+            bert_question_encoder, bert_paragraph_encoder, tokenizer,
+            hyper_params['max_question_len'], hyper_params['max_paragraph_len'], debug)
+    elif hyper_params['model']['name'] == 'bert_ffw':
+        bert_question_encoder = BertEncoder(hyper_params)
+        bert_paragraph_encoder = BertEncoder(hyper_params)
+        if bert_question_encoder.post_pooling_last_hidden_size != \
+                bert_paragraph_encoder.post_pooling_last_hidden_size:
+            raise ValueError("question/paragraph encoder should have the same output hidden size")
+        previous_hidden_size = bert_question_encoder.post_pooling_last_hidden_size
+        model = FeedForwardRetriever(
+            bert_question_encoder, bert_paragraph_encoder, tokenizer,
+            hyper_params['max_question_len'], hyper_params['max_paragraph_len'], debug,
+            hyper_params['model'], previous_hidden_size=previous_hidden_size)
+    elif hyper_params['model']['name'] == 'bert_cnn':
+        bert_question_encoder = BertEncoder(hyper_params)
+        bert_paragraph_encoder = BertEncoder(hyper_params)
+        if bert_question_encoder.post_pooling_last_hidden_size != \
+                bert_paragraph_encoder.post_pooling_last_hidden_size:
+            raise ValueError("question/paragraph encoder should have the same output hidden size")
+        previous_hidden_size = bert_question_encoder.post_pooling_last_hidden_size
+        model = CNNRetriever(
+            bert_question_encoder, bert_paragraph_encoder, tokenizer,
+            hyper_params['max_question_len'], hyper_params['max_paragraph_len'], debug,
+            hyper_params['model'], previous_hidden_size=previous_hidden_size)
+    elif hyper_params['model']['name'] == 'cnn':
+        cnn_question_encoder = CNNEncoder(hyper_params, tokenizer.vocab_size)
+        cnn_paragraph_encoder = CNNEncoder(hyper_params, tokenizer.vocab_size)
+        model = EmbeddingRetriever(
+            cnn_question_encoder, cnn_paragraph_encoder, tokenizer,
+            hyper_params['max_question_len'], hyper_params['max_paragraph_len'], debug)
+    else:
+        raise ValueError('model name {} not supported'.format(hyper_params['model']['name']))
+    return model
